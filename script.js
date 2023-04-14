0c78418ec7d7f293641a89a928456764da434e3f
Levid Lima <120687641+DEVLevid@users.noreply.github.com>
2023-04-14 18:54:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let password = document.querySelector(".password");
 eyeClosed.onclick = function(){
     if(password.type === "password"){
         password.type = "Text";
-        eyeClosed.src = "view.png";
+        eyeClosed.src = "images/view.png";
     }else{
         password.type = "password";
-        eyeClosed.src = "hide.png";
+        eyeClosed.src = "images/hide.png";
     }
 }
